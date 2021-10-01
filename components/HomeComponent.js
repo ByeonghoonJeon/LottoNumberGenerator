5a6079a6af90f91ed9067b478161ea2d1b854189
Nathan Jeon <jeon4972@snu.ac.kr>
2021-10-01 11:35:27 -0400

    Working on

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,55 +1,48 @@
-// import React, { Component } from "react";
-// import { View, Text, ScrollView } from "react-native";
-// import { Card } from "react-native-elements";
-// import { HISTORY } from "../shared/numberHistory.js";
-// import * as Animatable from "react-native-animatable";
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import { HISTORY } from "../shared/numberHistory.js";
+import * as Animatable from "react-native-animatable";
 
-// function RenderItem({ item }) {
-//   if (item) {
-//     return (
-//       <Card featuredTitle={item.round} image={item.basicImage}>
-//         <Text style={{ margin: 10 }}>
-//           {
-//             (item.number1,
-//             item.number2,
-//             item.number3,
-//             item.number4,
-//             item.number5,
-//             item.number6)
-//           }{" "}
-//           + {item.bonusNumber}
-//         </Text>
-//       </Card>
-//     );
-//   }
-//   return <View />;
-// }
+function RenderItem({ item }) {
+  if (item) {
+    return (
+      <Card>
+        <Text style={{ margin: 10 }}>
+          {item.round}
+          {item.number1},{item.number2},{item.number3},{item.number4},
+          {item.number5},{item.number6}+{item.bonusNumber}
+        </Text>
+      </Card>
+    );
+  }
+  return <View />;
+}
 
-// class Home extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       history: HISTORY,
-//     };
-//   }
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: HISTORY,
+    };
+  }
 
-//   static navigationOptions = {
-//     title: "My Chef",
-//   };
+  static navigationOptions = {
+    title: "My Chef",
+  };
 
-//   render() {
-//     return (
-//       <ScrollView>
-//         <Text>Hello</Text>
-//         {/* <Animatable.View animation="fadeInRightBig" duration={1000}>
-//           <RenderItem
-//             item={this.state.history[this.state.history.length - 1]}
-//             onPress={() => this.props.navigation.navigate("Contact")}
-//           />
-//         </Animatable.View> */}
-//       </ScrollView>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInRightBig" duration={1000}>
+          <RenderItem
+            item={this.state.history[0]}
+            onPress={() => this.props.navigation.navigate("Contact")}
+          />
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
+}
 
-// export default Home;
+export default Home;
